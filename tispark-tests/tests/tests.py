cdba331ac9d2dec28810b02216fb0f56804cda44
birdstorm <samuelwyf@hotmail.com>
2018-11-13 17:11:49 +0800

    Update TiSpark version to use Spark 2.3.2 (#44)
    * pin tispark to latest branch
    
    * fix pyspark tests on Travis CI
    
    * ignore pyspark test

--- tispark-tests/tests/tests.py
+++ tispark-tests/tests/tests.py
@@ -1,11 +1,8 @@
 from pyspark.sql import SparkSession
-import pytispark.pytispark as pti
 
 spark = SparkSession.builder.master("spark://tispark-master:7077").appName("TiSpark tests").getOrCreate()
 
-ti = pti.TiContext(spark)
- 
-ti.tidbMapDatabase("TPCH_001")
+spark.sql("use TPCH_001")
  
 count = spark.sql("select count(*) as c from lineitem").first()['c']
 
