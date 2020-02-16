c7b4d275503bba26ae271145e6d6294336ce4783
shuijing198799 <yinliang@pingcap.com>
2020-02-16 11:42:59 +0800

    deploy: fix bug

--- resign.sh
+++ resign.sh
@@ -8,9 +8,9 @@ sudo rm -rf data/*
 cd cert
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 
-echo '{"CN":"tidb-server","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="tidb,tikv0,tikv1,tikv2,pd0,pd1,pd2,127.0.0.1" - | cfssljson -bare tidb-server 
-echo '{"CN":"tikv-server","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="tidb,tikv0,tikv1,tikv2,pd0,pd1,pd2,127.0.0.1" - | cfssljson -bare tikv-server 
-echo '{"CN":"pd-server","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="tidb,tikv0,tikv1,tikv2,pd0,pd1,pd2,127.0.0.1" - | cfssljson -bare pd-server
+echo '{"CN":"tidb-server","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="tidb,127.0.0.1" - | cfssljson -bare tidb-server 
+echo '{"CN":"tikv-server","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="tikv0,tikv1,tikv2,127.0.0.1" - | cfssljson -bare tikv-server 
+echo '{"CN":"pd-server","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="pd0,pd1,pd2,127.0.0.1" - | cfssljson -bare pd-server
 
 echo '{"CN":"client","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=client -hostname="" - | cfssljson -bare client
 
